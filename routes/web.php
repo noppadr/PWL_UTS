c5fb1cc53206d86f1fabae99a2ffac7289c9bad4
Nova Diana <nopadr12@gmail.com>
2025-04-21 17:50:01 +0700

    web layout dan datatables kategori

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,14 @@ Route::prefix('level')->group(function () {
     Route::put('/{id}', [LevelController::class, 'update']);
     Route::delete('/{id}', [LevelController::class, 'destroy']);
 });
+
+Route::prefix('kategori')->group(function () {
+    Route::get('/', [KategoriController::class, 'index']);
+    Route::post('/list', [KategoriController::class, 'list']);
+    Route::get('/create', [KategoriController::class, 'create']);
+    Route::post('/', [KategoriController::class, 'store']);
+    Route::get('/{id}', [KategoriController::class, 'show']);
+    Route::get('/{id}/edit', [KategoriController::class, 'edit']);
+    Route::put('/{id}', [KategoriController::class, 'update']);
+    Route::delete('/{id}', [KategoriController::class, 'destroy']);
+});
